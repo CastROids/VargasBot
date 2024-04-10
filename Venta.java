75aedcbb8dd63646a4bba63072bce7c376ad0a4e
Multidisciplinario <160692048+CastROids@users.noreply.github.com>
2024-04-10 13:52:14 -0600

    Update Venta.java

--- Venta.java
+++ Venta.java
@@ -1,6 +1,6 @@
-
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.InputMismatchException;
 import java.time.LocalDate;
 
 public class Venta {
@@ -10,67 +10,61 @@ public class Venta {
     private ArrayList<Integer> listaCantidades = new ArrayList<>();
     private int idVenta;
     private LocalDate fecha;
-    private float precioTotal=0;
+    private float precioTotal = 0;
 
     public Venta(int idVenta) {
-        this.idVenta=idVenta;
-        fecha = LocalDate.now(); 
+        this.idVenta = idVenta;
+        fecha = LocalDate.now();
     }
 
-    
     public Scanner getCin() {
         return cin;
     }
-
 
     public ArrayList<Producto> getListaProductos() {
         return listaProductos;
     }
 
-
     public ArrayList<Integer> getListaCantidades() {
         return listaCantidades;
     }
-
 
     public int getIdVenta() {
         return idVenta;
     }
 
-
     public LocalDate getFecha() {
         return fecha;
     }
-
 
     public float getPrecioTotal() {
         return precioTotal;
     }
 
-
-    public float calcularPrecioTotal(){
-        for(int i = 0; i<listaProductos.size(); i++){
-            precioTotal += listaProductos.get(i).getPrecio()*listaCantidades.get(i);
+    public float calcularPrecioTotal() {
+        for (int i = 0; i < listaProductos.size(); i++) {
+            precioTotal += listaProductos.get(i).getPrecio() * listaCantidades.get(i);
         }
         return precioTotal;
     }
 
-    public void agregarProducto(Producto productoNuevo){
-        int cantidad=-1;
+    public void agregarProducto(Producto productoNuevo) throws InputMismatchException {
+        int cantidad = -1;
 
         listaProductos.add(productoNuevo);
         System.out.println("Ingrese la cantidad de unidadez que desea comprar de dicho producto");
         do {
             try {
                 cantidad = cin.nextInt();
-                if(cantidad<1){
+                if (cantidad < 1) {
                     System.out.println("Debe ingresar una cantidad valida mayor a 0, intente nuevamente\n");
                 }
             } catch (Exception e) {
-                cantidad=-1;
+                cantidad = -1;
                 System.out.println("Debe ingresar una cantidad válida: un valor numérico entero\n");
+                cin.next();
             }
-        } while (cantidad<1); 
+        } while (cantidad < 1);
 
         listaCantidades.add(cantidad);
     }
